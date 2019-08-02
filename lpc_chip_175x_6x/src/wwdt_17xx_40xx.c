55f229080f7996f79a508d4a2b81256a6ad6128e
akkipatel191996 <47867200+akkipatel191996@users.noreply.github.com>
2019-08-02 14:30:56 -0400

    Update wwdt_17xx_40xx.c

--- lpc_chip_175x_6x/src/wwdt_17xx_40xx.c
+++ lpc_chip_175x_6x/src/wwdt_17xx_40xx.c
@@ -5,7 +5,6 @@
  * Copyright(C) NXP Semiconductors, 2014
  * All rights reserved.
  *
- *
  * @par
  * Software that is described herein is for illustrative purposes only
  * which provides customers with programming information regarding the
